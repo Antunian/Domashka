88b44c6aec50f8582c9cd2bf69d519e47ae74c04
unknown <antunian007@gmail.com>
2023-09-23 19:30:07 +0300

    Domashka 2

---
+++ dom/dom_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int a1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int a2 = int.Parse(Console.ReadLine());
+int max = a;
+if (a1 > max) max = a1;
+if (a2 > max) max = a2;
+Console.WriteLine($"Наибольшее число {max}");
