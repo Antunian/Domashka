cbf97aa2696120d75a68e97fe37ecfac93b1a798
unknown <antunian007@gmail.com>
2023-09-23 21:49:16 +0300

    Domashka 3

---
+++ dom/dom_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int num = a % 2;
+Console.WriteLine(num);
+if (num == 0) Console.WriteLine($"Является четным число {a}");
+if (num == 1) Console.WriteLine($"Не является четным число {a}");
+
+
+
+
